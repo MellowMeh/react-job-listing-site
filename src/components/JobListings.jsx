69dd2c85d66c33e6ed429ccc98840497ccb3f5c2
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-11-06 13:35:37 -0500

    Add loading spinner

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
 import JobListing from "./JobListing";
+import Spinner from "./Spinner";
 
 const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
@@ -31,9 +32,15 @@ const JobListings = ({ isHome = false }) => {
             {isHome ? "Recent Jobs" : "Browse Jobs"}
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {jobs.map((job) => (
-              <JobListing key={job.id} job={job} />
-            ))}
+            {loading ? (
+              <Spinner loading={loading} />
+            ) : (
+              <>
+                {jobs.map((job) => (
+                  <JobListing key={job.id} job={job} />
+                ))}
+              </>
+            )}
           </div>
         </div>
       </section>
