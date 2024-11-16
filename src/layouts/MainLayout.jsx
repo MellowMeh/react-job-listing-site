498c7d7468564d96fdd4108656ba70f74619f96c
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-11-16 15:16:14 -0500

    Fix Navbar capitalization

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "../components/Navbar";
+import Navbar from "../components/Navbar";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css'
 
 const MainLayout = () => {
   return (
     <>
-      <NavBar />
+      <Navbar />
       <Outlet />
       <ToastContainer />
     </>
